9c64f6c2cb704f4e209eaa6770ad5d80f9fe2226
linjianguang <1419479392@qq.com>
2024-02-23 11:17:49 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # _promise
-根据 promises/A+ 实现的 promise ｜已通过官方所有测试用例
+根据 ```promises/A+``` 实现的 promise ｜ 已通过官方所有测试用例
+
+## 解析
+微信公众号：前端研习圈
+
+在 promise 系列中阅读相关文章
+
+## 安装依赖
+```
+pnpm install
+```
+
+## 执行官方测试用例
+```
+pnpm run test
+```
---
+++ core/Promise.js
@@ -0,0 +1,213 @@
+const PromiseState = {
+    pending: 'pending',
+    fulfilled: 'fulfilled',
+    rejected: 'rejected'
+}
+
+class _Promise {
+    state = PromiseState.pending;
+
+    value;   
+    reason;
+
+    fulfilledQueue = [];
+    rejectedQueue = [];
+
+    constructor(executor) {
+
+        try {
+            executor(
+                (value) => this.resolve(value),
+                (reason) => this.reject(reason)
+            )
+        } catch(e) {
+            this.reject(e);
+        }
+
+    }
+
+    resolve(value) {
+        if (this.state !== PromiseState.pending) {
+            return;
+        }
+
+        this.state = PromiseState.fulfilled;
+        this.value = value;
+
+        this.fulfilledQueue.forEach((onFulfilled) => onFulfilled(value))
+    }
+
+    reject(reason) {
+        if (this.state !== PromiseState.pending) {
+            return;
+        }
+
+        this.state = PromiseState.rejected;
+        this.reason = reason;
+
+        this.rejectedQueue.forEach((onRejected) => onRejected(reason))
+    }
+
+    resolutionProcedure(promise2, x, resolve, reject) {
+        if (promise2 === x) {
+            reject(new TypeError('promise2 === x'));
+            return;
+        }
+
+        if (x instanceof _Promise) {
+            x.then(
+                (value) => this.resolutionProcedure(promise2, value, resolve, reject),
+                (reason) => reject(reason)
+            );
+            return;
+        }
+
+        if (
+            x !== null &&
+            typeof x === 'object' ||
+            typeof x === 'function'
+        ) {
+            let then;
+
+            try {
+                then = x.then;
+            } catch (error) {
+                reject(error);
+                return;
+            }
+
+            if (typeof then === 'function') {
+                let isCalledResolvePromise = false;
+                let isCalledRejectPromise = false;
+                const resolvePromise = (value) => {
+                    if (isCalledResolvePromise || isCalledResolvePromise) {
+                        return;
+                    }
+
+                    isCalledResolvePromise = true;
+                    this.resolutionProcedure(promise2, value, resolve, reject)
+                }
+                const rejectPromise = (reason) => {
+                    if (isCalledRejectPromise || isCalledResolvePromise) {
+                        return;
+                    }
+
+                    isCalledRejectPromise = true;
+                    reject(reason)
+                }
+                try {
+                    then.call(
+                        x,
+                        resolvePromise,
+                        rejectPromise
+                    )
+                } catch (error) {
+                    if (
+                        !isCalledRejectPromise &&
+                        !isCalledResolvePromise
+                    ) {
+                        reject(error)
+                    }
+                }
+                return;
+            }
+        }
+
+        resolve(x);
+    }
+
+    resolveCallback(promise2, onFulfilled, resolve, reject) {
+        return (value) => queueMicrotask(() => {
+            if (typeof onFulfilled !== 'function') {
+                resolve(value);
+                return;
+            }
+
+            let x;
+
+            try {
+                x = onFulfilled(value);
+            } catch (error) {
+                reject(error);
+                return;
+            }
+
+            this.resolutionProcedure(promise2, x, resolve, reject)
+        })
+        
+    }
+
+    rejectCallback(promise2, onRejected, resolve, reject) {
+        return (reason) => queueMicrotask(() => {
+            if (typeof onRejected !== 'function') {
+                reject(reason);
+                return;
+            }
+
+            let x;
+
+            try {
+                x = onRejected(reason);
+            } catch (error) {
+                reject(error);
+                return;
+            }
+
+            this.resolutionProcedure(promise2, x, resolve, reject)
+        })
+        
+    }
+
+    then(onFulfilled, onRejected) {
+        const promise2 = new _Promise((resolve, reject) => {
+            queueMicrotask(() => {
+                onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : () => resolve(this.value);
+                onRejected = typeof onRejected === 'function' ? onRejected : () => reject(this.reason);
+
+                const resolveCallback = this.resolveCallback(promise2, onFulfilled, resolve, reject);
+                const rejectCallback = this.rejectCallback(promise2, onRejected, resolve, reject);
+
+                if (this.state === PromiseState.fulfilled) {
+                    resolveCallback(this.value);
+                    return;
+                }
+
+                if (this.state === PromiseState.rejected) {
+                    rejectCallback(this.reason);
+                    return;
+                }
+
+                this.fulfilledQueue.push(
+                    resolveCallback
+                )
+
+                this.rejectedQueue.push(
+                    rejectCallback
+                )
+            })
+        })
+
+        return promise2;
+    }
+}
+
+export default {
+    resolved(value) {
+        return new _Promise((resolve) => resolve(value))
+    },
+
+    rejected(reason) {
+        return new _Promise((_, reject) => reject(reason))
+    },
+
+    deferred() {
+        const ret = {};
+
+        ret.promise = new _Promise((resolve, reject) => {
+            ret.resolve = resolve;
+            ret.reject = reject
+        })
+
+        return ret;
+    }
+}
